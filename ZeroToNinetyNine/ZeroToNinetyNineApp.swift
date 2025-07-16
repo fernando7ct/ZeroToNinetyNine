984b846e2cb852db5a8b3c1f7579635175c28fa3
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-07-16 16:28:03 -0500

    Initial Commit

---
+++ ZeroToNinetyNine/ZeroToNinetyNineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroToNinetyNineApp.swift
+//  ZeroToNinetyNine
+//
+//  Created by Fernando Caudillo Tafoya on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeroToNinetyNineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
